2074ba4af0ae3c6c83bc682a73206dc7729e479b
Alexander Bird <alexkbird@gmail.com>
2016-01-30 10:53:50 -0700

    improvements to namespace demo: show global vs namespaced function

--- js/namespace.js
+++ js/namespace.js
@@ -3,8 +3,16 @@ var App = App || {};
 
 // Add a function to the namespace
 App.bedazzleTheWebsite = function() {
+  console.log("App.bedazzleTheWebsite");
   var boring = document.getElementById("boring");
   var someColors = ["green", "blue", "pink", "yellow", "red"];
   var randomColorIndex = parseInt(Math.random() * 5);
   boring.style.backgroundColor = someColors[randomColorIndex];
+}
+
+// No conflict with global bedazzleTheWebsite
+// Note: this is equivalent to `var bedazzleTheWebsite = function() {`
+function bedazzleTheWebsite() {
+  console.log("Global bedazzleTheWebsite");
+  document.body.style.backgroundColor = document.body.style.backgroundColor === "black" ? "white" : "black";
 }

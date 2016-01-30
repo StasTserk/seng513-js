2fcbf2983a8ad3d854da2a08f61273eca4cbccff
Alexander Bird <alexkbird@gmail.com>
2016-01-30 00:36:53 -0700

    retabbed namespace and revealing_module_pattern code

--- js/hello_world.js
+++ js/hello_world.js
@@ -1,18 +1,18 @@
 var App = App || {};
 App.HelloWorld = (function() {
-	function Greet(name) {
-		name = typeof name === "undefined" ? "stranger" : name;
+  function Greet(name) {
+    name = typeof name === "undefined" ? "stranger" : name;
     AddItem("Hi there, " + name);
-	}
+  }
 
   function AddItem(text) {
     var greeting = document.createElement("li");
     greeting.appendChild(document.createTextNode(text));
-		document.getElementById("container").appendChild(greeting);
+    document.getElementById("container").appendChild(greeting);
   }
 
-	return {
+  return {
     AddItem: AddItem,
-		Greet: Greet
-	}
+    Greet: Greet
+  }
 })();

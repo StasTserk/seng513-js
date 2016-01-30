9e143ed72bf69071a66ec8b3dc54d1ca03e457ec
Alexander Bird <alexkbird@gmail.com>
2016-01-30 00:46:16 -0700

    added details and comments to namespace and revealing_module examples

--- js/namespace.js
+++ js/namespace.js
@@ -1,5 +1,7 @@
+// Create namespace if it doesn't exist yet
 var App = App || {};
 
+// Add a function to the namespace
 App.bedazzleTheWebsite = function() {
   var boring = document.getElementById("boring");
   boring.style.backgroundColor = "green";
--- js/revealing_module.js
+++ js/revealing_module.js
@@ -1,16 +1,28 @@
 var App = App || {};
 App.HelloWorld = (function() {
+  // Private variable
+  var startOfGreeting = "Hi there, ";
+
+  // Public function
   function Greet(name) {
     name = typeof name === "undefined" ? "stranger" : name;
-    AddItem("Hi there, " + name);
+    AddItem(startOfGreeting + name);
   }
 
+  // Public function
   function AddItem(text) {
-    var greeting = document.createElement("li");
-    greeting.appendChild(document.createTextNode(text));
-    document.getElementById("container").appendChild(greeting);
+    document.getElementById("container").appendChild(createListElement(text));
   }
 
+  // Private function
+  function createListElement(text) {
+    var listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(text));
+    return listItem
+  }
+
+  // Create an object liternal containing references to the functions that 
+  //  should be public, and return it
   return {
     AddItem: AddItem,
     Greet: Greet

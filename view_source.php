5df14c766a231931bcb12da66e16b47363e48cb1
Alexander Bird <alexkbird@gmail.com>
2016-01-30 11:04:29 -0700

    view_source only prints html files, not PHP

--- view_source.php
+++ view_source.php
@@ -1,7 +1,10 @@
 <pre>
   <?php if(isset($_GET['pagename'])) {
+    // don't allow browsing into subdirectories - just look in the current directory
     $filename = basename($_GET['pagename']);
-    if(file_exists($filename)) {
+
+    // print if if it exists and isn't PHP
+    if(file_exists($filename) && pathinfo($filename, PATHINFO_EXTENSION) != "php") {
       $contents = file_get_contents($filename);
       echo highlight_string($contents, true);
     }
